fde6720e0288922802a6f9c167762e4a7e4918cf
Stefomat <41241946+Stefomat@users.noreply.github.com>
2023-04-04 12:13:06 +0200

    Create PuTTY2BC.ps1

---
+++ PuTTY2BC.ps1
@@ -0,0 +1,13 @@
+$incre = 0
+
+foreach($p in Get-ChildItem "HKCU:\SOFTWARE\SimonTatham\PuTTY\Sessions" -Name) {
+  Write-Host "Session $p"
+  & 'C:\Program Files\Beyond Compare 4\BCompare.exe' /ro "reg:\\HKEY_CURRENT_USER\SOFTWARE\SimonTatham\PuTTY\Sessions\Default%20Settings" "reg:\\HKEY_CURRENT_USER\SOFTWARE\SimonTatham\PuTTY\Sessions\$p"
+  $incre++
+  
+  if($incre -ge 20){
+    Read-Host -Prompt "20 Sessions, Enter to continue..."
+    $incre = 0
+  }
+}
+Write-Host "Done"
